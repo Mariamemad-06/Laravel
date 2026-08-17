dfd050016e78f92351e51ab7b5ea112101751653
Mariam emadeldin <mariam.emadeldin.saad.2387@gmail.com>
2026-08-17 10:33:07 +0300

    controllers

---
+++ app/Http/Controllers/WarehouseController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Warehouse ;
+
+class WarehouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $warehouse = Warehouse::paginate(10);
+        return $warehouse;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+         $request->validate([
+            'name' => 'required|string|max:255',
+            'location' => 'nullable|string',
+        ]);
+        $warehouse = Warehouse::create($request->all());
+        return response()->json([
+            'message' => 'Warehouse created successfully',
+            'warehouse' => $warehouse
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Warehouse $warehouse)
+    {
+        return $warehouse;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $warehouse = Warehouse::findOrFail($id);
+
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'location' => 'nullable|string',
+        ]);
+
+        $warehouse->update($request->all());
+
+        return response()->json([
+            'message' => 'Warehouse updated successfully',
+            'warehouse' => $warehouse
+        ]);
+    }
+   
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Warehouse $warehouse)
+    {
+        $warehouse->delete();
+
+        return response()->json([
+            'message' => 'Warehouse deleted successfully',
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WarehouseController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +16,9 @@ Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::put('/products/{id}',[ProductController::class,'update']);
 Route::delete('/products',[ProductController::class,'delete']);
+
+Route::get('/warehouses', [WarehouseController::class, 'index']);
+Route::post('/warehouses', [WarehouseController::class,'store']);
+Route::put('/warehouses/{id}', [WarehouseController::class,'update']);
+Route::get('/warehouses/{id}', [WarehouseController::class,'show']);
+Route::delete('/warehouses/{id}', [WarehouseController::class,'destroy']);
